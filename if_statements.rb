42b6fb6b6384ea486c29189e0c849f5166d4acfb
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-06 15:36:45 -0400

    Finishes the last section on pizza making.

--- if_statements.rb
+++ if_statements.rb
@@ -65,5 +65,11 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sauce
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
